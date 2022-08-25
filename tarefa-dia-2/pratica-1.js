0cb9e645f5c4c44cfe47a8ddce4f5ebb46ca6e08
Luciano Santos <lucianotecladista@gmail.com>
2022-08-24 23:53:35 -0300

    add template string pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,6 +8,14 @@ let idade= 42
 console.log(idade)
 let viagemdossonhos= "Japao"
 console.log(viagemdossonhos)
+let confirmacaodecompra= ` Olá, ${nome}!
+ Somos aqui da agência de turismo Viagem dos Sonhos.
+ Estamos te escrevendo este email,pois acabamos de confirmar as compras das passagens aéreas para ${viagemdossonhos}.
+ Confirma pra gente alguns dados?
+    A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}?
+    Ficamos no aguardo. Muito obrigado e boa viagem! :D\n`
+console.log(confirmacaodecompra)
+
 
 
 

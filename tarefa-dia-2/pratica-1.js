078839ecfeadc72246c7faea9f9a6b6d76d5894a
Luciano Santos <lucianotecladista@gmail.com>
2022-08-24 23:10:43 -0300

    Add console pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,8 +1,14 @@
 let nome= "Luciano"
+console.log(nome)
 let email= "lucianotecladista@gmail.com"
+console.log(email)
 let profissao= "Bancario"
+console.log(profissao)
 let idade= 42
+console.log(idade)
 let viagemdossonhos= "Japao"
+console.log(viagemdossonhos)
+
 
 
 

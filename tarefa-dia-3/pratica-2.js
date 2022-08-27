4996e97e0c33b5e680a53df0dd799d5eee4b7d39
Luciano Santos <lucianotecladista@gmail.com>
2022-08-27 16:35:31 -0300

    Alteração pratica-2

--- tarefa-dia-3/pratica-2.js
+++ tarefa-dia-3/pratica-2.js
@@ -1,6 +1,9 @@
 let multiplicador = 9
 let n = 10
 let i = 1
+let nomeDaTabuada = "Tabuada de"
+
+console.log(nomeDaTabuada, multiplicador)
 
 while (i <= n) {
     console.log(`${multiplicador} X ${1} = ${multiplicador * i}`)
